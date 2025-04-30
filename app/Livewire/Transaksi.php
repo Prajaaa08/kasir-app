991a5445564af3b4e041f4ac2220d1646de99336
Prajaaa08 <lestariaagustina@gmail.com>
2025-04-30 08:38:06 +0700

    Menambahkan Fitur Masukkan Kode Untuk Transaksi

--- app/Livewire/Transaksi.php
+++ app/Livewire/Transaksi.php
@@ -1,13 +1,61 @@
 <?php
 
 namespace App\Livewire;
-
+use App\Models\DetilTransaksi;
+use App\Models\Produk;
+use App\Models\Transaksi as ModelTransaksi;
 use Livewire\Component;
 
 class Transaksi extends Component
 {
+    public $kode, $total, $status, $bayar, $kembalian, $totalSemuaBelanja;
+    public $transaksiAktif;
+
+    public function transaksiBaru()
+    {
+        $this->reset();
+        $this->transaksiAktif = new ModelTransaksi();
+        $this->transaksiAktif->kode = 'INV/' . date('YmdHis');
+        $this->transaksiAktif->total = 0;
+        $this->transaksiAktif->status = 'pending';
+        $this->transaksiAktif->save();
+    }
+
+    public function batalTransaksi()
+    {
+        if ($this->transaksiAktif) {
+            $detilTransaksi = DetilTransaksi::where('transaksi_id', $this->transaksiAktif->id)->get();
+            foreach ($detilTransaksi as $detil) {
+                $detil->delete();
+            }
+            $this->transaksiAktif->delete();
+        }
+        $this->reset();
+    }
+
+    public function updatedKode()
+    {
+        $produk = Produk::where('kode', $this->kode)->first();
+        if ($produk && $produk->stok > 0) {
+            $detil = DetilTransaksi::firstOrNew(
+                ['transaksi_id' => $this->transaksiAktif->id, 'produk_id' => $produk->id],
+                ['jumlah' => 0]
+            );
+            $detil->jumlah += 1;
+            $detil->save();
+            $this->reset('kode');
+        }
+    }
     public function render()
     {
-        return view('livewire.transaksi');
+        if ($this->transaksiAktif) {
+            $semuaProduk = DetilTransaksi::where('transaksi_id', $this->transaksiAktif->id)->get();
+        } else {
+            $semuaProduk = [];
+        }
+
+        return view('livewire.transaksi')->with([
+            'semuaProduk' => $semuaProduk
+        ]);
     }
 }
--- resources/views/livewire/transaksi.blade.php
+++ resources/views/livewire/transaksi.blade.php
@@ -2,60 +2,82 @@
     <div class="container">
         <div class="row my-2">
             <div class="col-12">
-                <button class="btn btn-primary">Transaksi Baru</button>
-                <button class="btn btn-danger">Batalkan Transaksi</button>
+                @if (!$transaksiAktif)
+                    <button class="btn btn-primary" wire:click='transaksiBaru'>Transaksi Baru</button>
+                @else
+                    <button class="btn btn-danger" wire:click='batalTransaksi'>Batalkan Transaksi</button>
+                @endif
                 <button class="btn btn-info" wire:loading>Loading ...</button>
             </div>
         </div>
-        <div class="row mt-2">
-            <div class="col-8">
-                <div class="card border-primary">
-                    <div class="card-body">
-                        <h4 class="card-title">No Invoice :</h4>
-                        <table class="table table-bordered" >
-                            <thead>
-                                <tr>
-                                    <th>No</th>
-                                    <th>Nama Barang</th>
-                                    <th>Harga</th>
-                                    <th>Qty</th>
-                                    <th>Total</th>
-                                    <th>Aksi</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-            </div>
-            <div class="col-4">
-                <div class="card border-primary">
-                    <div class="card-body">
-                        <h4 class="card-title">Total Biaya</h4>
-                        <div class="flex justify-content-betweeen">
-                            <span>Rp.</span>
-                            <span>{{ number_format('9999999',2,'.',',') }}</span>
+        @if ($transaksiAktif)
+            <div class="row mt-2">
+                <div class="col-8">
+                    <div class="card border-primary">
+                        <div class="card-body">
+                            <h4 class="card-title">No Invoice :</h4>
+                            <input type="string" class="form-control" placeholder="Masukkan kode invoice" wire:model.live='kode'>
+                            <table class="table table-bordered">
+                                <thead>
+                                    <tr>
+                                        <th>No</th>
+                                        <th>Nama Barang</th>
+                                        <th>Harga</th>
+                                        <th>Qty</th>
+                                        <th>Total</th>
+                                        <th>Aksi</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @foreach ($semuaProduk as $produk)
+                                        <tr>
+                                            <td>{{ $loop->iteration }}</td>
+                                            <td>{{ $produk->produk->kode}}</td>
+                                            <td>{{ $produk->produk->nama}}</td>
+                                            <td>{{ number_format($produk->produk->harga, 2, '.', ',') }}</td>
+                                            <td>
+                                                {{$produk->jumlah}}
+                                            </td>
+                                            <td>{{ number_format($produk->produk->harga *  $produk->jumlah,2, '.', ',') }}</td>
+                                            <td>
+                                                <button wire:click="hapusProduk({{ $produk->id }})"
+                                                    class="btn btn-danger">Hapus</button>
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
                         </div>
                     </div>
                 </div>
-                <div class="card border-primary mt-2">
-                    <div class="card-body">
-                        <h4 class="card-title">Bayar</h4>
-                        <input type="number" class="form-control" placeholder="Masukkan jumlah bayar">
-                    </div>
-                </div>
-                <div class="card border-primary mt-2">
-                    <div class="card-body">
-                        <h4 class="card-title">Kembalian</h4>
-                        <div class="flex justify-content-betweeen">
-                            <span>Rp.</span>
-                            <span>{{ number_format('9999999',2,'.',',') }}</span>
+                <div class="col-4">
+                    <div class="card border-primary">
+                        <div class="card-body">
+                            <h4 class="card-title">Total Biaya</h4>
+                            <div class="flex justify-content-betweeen">
+                                <span>Rp.</span>
+                                <span>{{ number_format('9999999', 2, '.', ',') }}</span>
+                            </div>
                         </div>
                     </div>
+                    <div class="card border-primary mt-2">
+                        <div class="card-body">
+                            <h4 class="card-title">Bayar</h4>
+                            <input type="number" class="form-control" placeholder="Masukkan jumlah bayar">
+                        </div>
+                    </div>
+                    <div class="card border-primary mt-2">
+                        <div class="card-body">
+                            <h4 class="card-title">Kembalian</h4>
+                            <div class="flex justify-content-betweeen">
+                                <span>Rp.</span>
+                                <span>{{ number_format('9999999', 2, '.', ',') }}</span>
+                            </div>
+                        </div>
+                    </div>
+                    <button class="btn btn-success w-100 mt-2">Bayar</button>
                 </div>
-                <button class="btn btn-success w-100 mt-2">Bayar</button>
             </div>
-        </div>
+        @endif
     </div>
 </div>
