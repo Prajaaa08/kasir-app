f616a7cffdde9b2834f5e50c456d09ae6319bf06
Prajaaa08 <lestariaagustina@gmail.com>
2025-05-14 07:50:53 +0700

    Menambahkan Fitur Cetak Dan Laporan Transaksi

--- resources/views/livewire/laporan.blade.php
+++ resources/views/livewire/laporan.blade.php
@@ -5,6 +5,7 @@
                 <div class="card border-primary">
                     <div class="card-body">
                         <h4 class="card-title">Laporan Transaksi</h4>
+                        <a href="{{ route('cetak') }}" target="_blank">CETAK</a>
 
                         <table class="table table-bordered table-striped table-hover" id="laporan">
                             <thead>
@@ -14,7 +15,14 @@
                                 <th>total</th>
                             </thead>
                             <tbody>
-
+                                @foreach ($semuaTransaksi as $transaksi)
+                                <tr>
+                                    <td>{{ $loop->iteration }}</td>
+                                    <td>{{ $transaksi->created_at->format('d-m-Y') }}</td>
+                                    <td>{{ $transaksi->kode }}</td>
+                                    <td>Rp. {{ number_format($transaksi->total, 0, ',', '.') }}</td>
+                                </tr>
+                                @endforeach
                             </tbody>
                         </table>
                     </div>
