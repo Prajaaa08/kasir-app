667ee1a9b2690389308ec10ad58d15f6b2e5594d
Prajaaa08 <lestariaagustina@gmail.com>
2025-05-14 07:25:19 +0700

    Membuat Tampilan Laporan

--- resources/views/livewire/laporan.blade.php
+++ resources/views/livewire/laporan.blade.php
@@ -1,3 +1,25 @@
 <div>
-    {{-- In work, do what you enjoy. --}}
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="card border-primary">
+                    <div class="card-body">
+                        <h4 class="card-title">Laporan Transaksi</h4>
+
+                        <table class="table table-bordered table-striped table-hover" id="laporan">
+                            <thead>
+                                <th>No</th>
+                                <th>Tanggal</th>
+                                <th>No Inv.</th>
+                                <th>total</th>
+                            </thead>
+                            <tbody>
+
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
